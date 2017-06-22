091b0977351738e034fda456e7b3bfa67887ac19
yaosikai <1165524245@qq.com>
2017-06-22 18:02:56 +0800

    与服务器同步

--- Rusys.sh
+++ Rusys.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #vas2xyz.pl ts.vasp
-moviecombine.pl $1 $1 12.xyz -13.529 0 0
-moviecombine.pl 12.xyz $1 123.xyz 13.529 0 0
-moviecombine.pl 123.xyz 123.xyz 123123.xyz -5.4116 9.3732 0
-moviecombine.pl 123123.xyz 123.xyz final.xyz 5.4116 -9.3732 0
+moviecombine.pl $1 $2 12.xyz 9.9550820148943 0 0
+moviecombine.pl 12.xyz $2 123.xyz -9.9550820148943 0 0
+moviecombine.pl 123.xyz 123.xyz 123123.xyz -4.9775410074471500 8.6213539216560395 0
+moviecombine.pl 123123.xyz 123.xyz final.xyz 4.9775410074471500 -8.6213539216560395 0
 rm 12.xyz 123.xyz 123123.xyz
 mv final.xyz $1
--- temp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mv CONTCAR POSCAR
-dir2car.pl POSCAR
-mv POSCAR-C.vasp POSCAR
-sed -i '28c\  EDIFFG = -0.01' sub
-clc
-sub sub
-
-
